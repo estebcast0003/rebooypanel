276948a7a147de000bc2bbcf68dc5c1c1334d41d
Gayo Mcennis <gayomcennis@gmail.com>
2026-09-12 17:56:47 -0700

    feat(ui): add creator favicon, sidebar brand identity and password visibility toggle

--- config/urls.py
+++ config/urls.py
@@ -20,9 +20,13 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.views.generic import RedirectView
 from core import views as core_views
 
 urlpatterns = [
+    # Favicon
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'img/favicon.ico', permanent=True)),
+
     # Home
     path('', core_views.home_view, name='home'),
 
